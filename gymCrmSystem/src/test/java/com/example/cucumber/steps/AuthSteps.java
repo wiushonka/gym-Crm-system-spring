7839607382db974afb9ae3a91bbc504b1ec7c8a9
wiushonka <bagrato2004@gmail.com>
2025-11-18 22:26:37 +0100

    added test for exceeding maximum allowed failed login attempts

--- gymCrmSystem/src/test/java/com/example/cucumber/steps/AuthSteps.java
+++ gymCrmSystem/src/test/java/com/example/cucumber/steps/AuthSteps.java
@@ -1,19 +1,28 @@
 package com.example.cucumber.steps;
 
 import io.cucumber.java.en.*;
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.*;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.Map;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class AuthSteps {
 
+    private static final Logger log = LoggerFactory.getLogger(AuthSteps.class);
     private final RestTemplate restTemplate = new RestTemplate();
     private ResponseEntity<String> response;
     @LocalServerPort
     private int port;
     private String username;
     private String password;
+    private Response resp;
 
     @Given("I have valid credentials")
     public void i_have_valid_credentials() {
@@ -45,5 +54,32 @@ public class AuthSteps {
         String jwtToken = body.substring(body.indexOf(":\"") + 2, body.lastIndexOf("\""));
         assertTrue(jwtToken.length() > 0);
         System.out.println("JWT token: " + jwtToken);
+    }
+
+    @Given("I have invalid credintals username: {string} and password: {string}")
+    public void i_have_invalid_credintals(String testUsername, String testIncorrectPassword) {
+        username = testUsername;
+        password = testIncorrectPassword;
+    }
+
+    @When("I try to login to endpoint {string} with incorrect credintals many times")
+    public void i_try_to_login_to_endpoint_with_incorrect_credintals(String endpoint) {
+        Map<String,String> invalidCredintals = Map.of(
+            "username",username,
+            "password",password
+        );
+
+        for(int i=0;i<=4;++i){
+            resp = RestAssured.given().contentType("application/json")
+                    .body(invalidCredintals)
+                    .post("http://localhost:"+ port + endpoint);
+        }
+    }
+
+    @Then("after trying it more than 3 timess I should recive message telling me that i got banned")
+    public void did_i_get_banned_or_not() {
+        log.info(resp.prettyPrint());
+        String message = resp.jsonPath().getString("message");
+        assertTrue(message.contains("locked"));
     }
 }
--- gymCrmSystem/src/test/resources/features/auth.feature
+++ gymCrmSystem/src/test/resources/features/auth.feature
@@ -5,3 +5,8 @@ Feature: Authentication API
     When I send a POST request to login endpoint "/api/v1/auth/login"
     Then I should receive a 200 response
     And the response should contain a JWT tokenlid credintals
+
+  Scenario: Unsuccessful login more than 3 times blocks user
+    Given I have invalid credintals username: "sarahw" and password: "mistake"
+    When I try to login to endpoint "/api/v1/auth/login" with incorrect credintals many times
+    Then after trying it more than 3 timess I should recive message telling me that i got banned
